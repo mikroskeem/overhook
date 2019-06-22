dd0da28a1af151698f0712a6667a0f022ef2c6e0
Erik Allik <eallik@gmail.com>
2019-06-22 17:01:28 +0300

    fix n00b fail :/

--- src/overhook/core.clj
+++ src/overhook/core.clj
@@ -9,16 +9,9 @@
                                       :body text})
 
 (defn github-webhook-handler [req]
-  (let [github-signature (-> req :headers :X-Hub-Signature)]
-    (let [resp (
-      (when (nil? github-signature)
-        simple-response 405, "nope")
-      (when-not (nil? github-signature)
-        (let [body (-> req :body)]
-          (println body)
-          simple-response 200, "OK")))]
-      (println resp)
-      resp)))
+  (if (-> req :headers :X-Hub-Signature)
+    (simple-response 405 "nope")
+    (simple-response 200 "OK")))
 
 (defroutes all-routes
   (GET "/" [] (simple-response 200 "nothing here"))
@@ -32,6 +25,7 @@
     (reset! server nil)))
 
 (defn -main [& args]
+  (println "foo")
   (reset! server (run-server #'all-routes {:ip "0.0.0.0"
-                                    :port 8080
-                                    :thread 2})))
+                                           :port 8080
+                                           :thread 2})))
