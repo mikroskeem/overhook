fbfcc299d0cf5cb6a131962d7f269f4c95cf7e7c
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-06 21:39:09 +0300

    Fix hexencode function

--- src/overhook/core.clj
+++ src/overhook/core.clj
@@ -11,7 +11,7 @@
 
 (defn hexencode [b]
   (apply str
-    (map #(format "%02x" (int %)) b)))
+    (map #(format "%02x" (int (bit-and % 0xFF))) b)))
 
 (defn simple-response [status, text] {:status status
                                       :headers {"Content-Type" "text/plain"}
@@ -24,17 +24,18 @@
       (.doFinal mac
         (.bytes contents))))) ; org.httpkit.BytesInputStream specific
 
-(defn verify-github-signature? [secret, contents, expected_signature]
+(defn verify-github-signature? [secret, contents, expected-signature]
   (let [digest (str "sha1=" (hmac-sha1-hexdigest secret contents))]
     (MessageDigest/isEqual
       (.getBytes digest StandardCharsets/UTF_8)
-      (.getBytes expected_signature StandardCharsets/UTF_8))))
+      (.getBytes expected-signature StandardCharsets/UTF_8))))
 
 (defn github-webhook-handler [req]
   (if-let [github-signature ((req :headers) "x-hub-signature")]
     (if (verify-github-signature? github-secret-key (req :body) github-signature)
       (do
         ; TODO: Pass to Discord
+        (log/info "Got valid GitHub request!")
         (simple-response 200 "OK"))
       (do
         (log/info "Remote" (req :remote-addr) "sent invalid body (signature does not match)")
