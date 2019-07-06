6f73f5f9f0abbe3d48e5c2b9d6900b7904021714
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-06 18:33:32 +0300

    Commit undone changes

--- src/overhook/core.clj
+++ src/overhook/core.clj
@@ -2,16 +2,40 @@
   (:use [compojure.route :only [not-found]]
         [compojure.core :only [defroutes GET POST context]]
         org.httpkit.server)
+  (:import (javax.crypto Mac)
+           (javax.crypto SecretKeySpec)
+           (java.security MessageDigest)
+           (java.nio.charset StandardCharsets)
   (:gen-class))
+
+(def github-secret-key "1234567890")
+
+(defn hexencode [b]
+  (apply str
+    (map #(format "%02x" (int %)) b)))
 
 (defn simple-response [status, text] {:status status
                                       :headers {"Content-Type" "text/plain"}
                                       :body text})
 
+(defn hmac-sha1-hexdigest [secret, contents]
+  (let [key-spec (SecretKeySpec. (.getBytes secret) "HmacSHA1")
+        mac (doto (Mac/getInstance "HmacSHA1") (.init key-spec))]
+    (hexencode
+      (.doFinal mac
+        (.getBytes contents)))))
+
+(defn verify-github-signature? [secret, contents, expected_signature]
+  (let [digest (str "sha1=" (hmac-sha1-hexdigest secret, contents))]
+    (MessageDigest/isEqual (.getBytes digest StandardCharsets/UTF_8) (.getBytes expected_signature StandardCharsets/UTF_8))))
+
 (defn github-webhook-handler [req]
-  (if-let [github-signature (-> req :headers :X-Hub-Signature)]
+  (if-let [github-signature (-> req :headers :X-Hub-Signature)
+           body (-> req :body)]
     (simple-response 403 "nope")
-    (simple-response 200 "OK")))
+    (if (verify-github-signature? github-secret-key body github-signature)
+      (simple-response 200 "OK")
+      (simple-response 403 "nope"))
 
 (defroutes all-routes
   (GET "/" [] (simple-response 200 "nothing here"))
