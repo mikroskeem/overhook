9991e3b200531aefee65b45613543d87974bb1f9
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-07 22:21:33 +0300

    Attempt to turn Overhook into native image

--- project.clj
+++ project.clj
@@ -5,6 +5,12 @@
             :url "https://opensource.org/licenses/MIT"
             :key "mit"
             :year 2019}
+  :plugins [[io.taylorwood/lein-native-image "0.3.0"]]
+  :native-image {:name "overhook"
+                 :opts ["--verbose"
+                        "--initialize-at-build-time"
+                        "--enable-url-protocols=http,https"
+                        "--report-unsupported-elements-at-runtime"]}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "0.2.6"]
                  [cprop "0.1.14"]
@@ -12,4 +18,6 @@
                  [metosin/reitit-ring "0.3.9"]]
   :main overhook.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot
+                       :all
+                       :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}})
